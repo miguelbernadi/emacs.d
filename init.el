9f6749d973cbf39f9750302f81aaa0d94331e9b6
Miguel Bernabeu <miguel.bernabeu@devex.com>
2017-01-27 19:56:29 +0100

    Redo org-agenda directories

--- init.el
+++ init.el
@@ -18,13 +18,13 @@
 (tool-bar-mode -1)
 
 ;;; Set agenda files
-(setq org-agenda-files (list "~/org/test.org"))
+(setq org-agenda-files (list "~/org/agenda/test.org"))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/org/newgtd.org"))))
+ '(org-agenda-files (quote ("~/org/agenda/newgtd.org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
