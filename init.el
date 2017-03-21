172c78290e6ea293f5d980d42e951fc1982f79cc
Miguel Bernabeu <miguel.bernabeu@devex.com>
2017-03-21 03:04:58 +0100

    Unify go mode configuration

--- init.el
+++ init.el
@@ -29,7 +29,41 @@
   (add-hook 'after-init-hook #'global-flycheck-mode))
 
 (use-package go-mode
-	     :ensure t)
+  :ensure t
+  :config
+  ;;; Get PATH environment
+  ;;; (https://tleyden.github.io/blog/2014/05/22/configure-emacs-as-a-go-editor-from-scratch/)
+  (defun set-exec-path-from-shell-PATH ()
+    (let ((path-from-shell (replace-regexp-in-string
+			    "[ \t\n]*?"
+			    ""
+			    (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+      (setenv "PATH" path-from-shell)
+      (setq eshell-path-env path-from-shell) ; for eshell
+      (setq exec-path (split-string path-from-shell path-separator))))
+
+  (when window-system (set-exec-path-from-shell-PATH))
+
+  ;;; Set go related paths
+  (setenv "GOPATH" "/Users/mbernabe/go")
+  (add-to-list 'exec-path "/Users/mbernabe/go/bin")
+
+  (defun my-go-mode-hook ()
+    ;; Use goimports instead of go-fmt
+    (setq gofmt-command "goimports")
+    ;; Call gofmt before saving
+    (add-hook 'before-save-hook 'gofmt-before-save)
+    ;; Customize compile command to run go build
+    (if (not (string-match "go" compile-command))
+	(set (make-local-variable 'compile-command)
+	     "go build -v && go test -v && go vet"))
+    )
+  (add-hook 'go-mode-hook 'my-go-mode-hook)
+
+  (defun auto-complete-for-go ()
+    (auto-complete-mode 1))
+  (add-hook 'go-mode-hook 'auto-complete-for-go))
+
 (use-package exec-path-from-shell
 	     :ensure t)
 (use-package go-errcheck
@@ -87,36 +121,3 @@
 ;; Display line and column numbers in Mode line
 (setq line-number-mode t)
 (setq column-number-mode t)
-
-;;; Get PATH environment
-;;; (https://tleyden.github.io/blog/2014/05/22/configure-emacs-as-a-go-editor-from-scratch/)
-(defun set-exec-path-from-shell-PATH ()
-  (let ((path-from-shell (replace-regexp-in-string
-			  "[ \t\n]*?"
-			  ""
-			  (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
-    (setenv "PATH" path-from-shell)
-    (setq eshell-path-env path-from-shell) ; for eshell
-    (setq exec-path (split-string path-from-shell path-separator))))
-
-(when window-system (set-exec-path-from-shell-PATH))
-
-;;; Set go related paths
-(setenv "GOPATH" "/Users/mbernabe/go")
-(add-to-list 'exec-path "/Users/mbernabe/go/bin")
-
-(defun my-go-mode-hook ()
-  ;; Use goimports instead of go-fmt
-  (setq gofmt-command "goimports")
-  ;; Call gofmt before saving
-  (add-hook 'before-save-hook 'gofmt-before-save)
-  ;; Customize compile command to run go build
-  (if (not (string-match "go" compile-command))
-    (set (make-local-variable 'compile-command)
-	 "go build -v && go test -v && go vet"))
-  )
-(add-hook 'go-mode-hook 'my-go-mode-hook)
-
-(defun auto-complete-for-go ()
-  (auto-complete-mode 1))
-(add-hook 'go-mode-hook 'auto-complete-for-go)
