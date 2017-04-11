4d307a2f5d807093612bf3bc18caf4ba982641ae
Miguel Bernabeu <miguel.bernabeu@devex.com>
2017-04-11 21:41:43 +0200

    Add mu4e with configuration

--- init.el
+++ init.el
@@ -13,6 +13,9 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
+;; Add locally installed package paths here
+(add-to-list 'load-path "/usr/local/opt/mu/share/emacs/site-lisp/mu/mu4e/")
+
 ;;; Install packages I want
 (eval-when-compile
   (require 'use-package))
@@ -118,6 +121,92 @@
 (use-package exec-path-from-shell
   :ensure t)
 
+(use-package mu4e
+  :config
+  ;; Set mu4e as default mail client for emacs
+  (setq mail-user-agent 'mu4e-user-agent)
+  ;; Mu4e program settings
+  (setq mu4e-mu-binary "/usr/local/bin/mu"
+	mu4e-get-mail-command "offlineimap"
+	mu4e-update-interval 300) ;; seconds between auto-update
+
+  ;; Folder settings and behaviour
+  (setq	mu4e-maildir "~/Mail"
+	mu4e-drafts-folder "/[Gmail].Drafts"
+	mu4e-sent-folder   "/[Gmail].Sent Mail"
+	mu4e-trash-folder  "/[Gmail].Trash"
+	;; don't save message to Sent Messages, Gmail/IMAP takes care
+	;; of this (See the documentation for
+	;; `mu4e-sent-messages-behavior' if you have additional
+	;; non-Gmail addresses and want assign them different
+	;; behavior.)
+	mu4e-sent-messages-behavior 'delete)
+
+  ;; Navigation settings
+  
+  ;; setup some handy shortcuts you can quickly
+  ;; switch to your Inbox -- press ``ji'' then, when you want archive
+  ;; some messages, move them to the 'All Mail' folder by pressing
+  ;; ``ma''.
+  (setq mu4e-maildir-shortcuts
+	'( ("/INBOX"               . ?i)
+	   ("/[Gmail].Sent Mail"   . ?s)
+	   ("/[Gmail].Trash"       . ?t)
+	   ("/[Gmail].All Mail"    . ?a)))
+
+  ;; This variable can be used to configure the bookmarks in the main view
+  (setq mu4e-bookmarks
+	`( ,(make-mu4e-bookmark
+	     :name  "Unread messages"
+	     :query "flag:unread AND NOT flag:trashed"
+	     :key ?u)
+	   ,(make-mu4e-bookmark
+	     :name "Today's messages"
+	     :query "date:today..now"
+	     :key ?t)
+	   ,(make-mu4e-bookmark
+	     :name "Last 7 days"
+	     :query "date:7d..now"
+	     :key ?w)
+	   ,(make-mu4e-bookmark
+	     :name "Flagged in INBOX"
+	     :query "maildir:\"/INBOX\" and flag:flagged"
+	     :key ?f)))
+
+  ;; Editor settings
+  ;; every new email composition gets its own frame! (window)
+  (setq mu4e-compose-in-new-frame t
+	;; Avoid self replies in Reply to all
+	mu4e-compose-dont-reply-to-self t)
+
+  ;; User personal information
+  (setq mu4e-user-mail-address-list '("miguel.bernabeu@devex.com")
+	user-mail-address "miguel.bernabeu@gmail.com"
+	user-full-name  "Miguel Bernabeu"
+	mu4e-compose-signature
+	(concat
+	 "Miguel Bernabeu\n"
+	 "Infrastructure Engineer @ Devex\n"))
+
+  ;; Headers View settings
+  ;; Set date format
+  (setq mu4e-headers-date-format "%Y-%m-%d %H:%M"
+  ;; Set fields in Headers view
+	mu4e-headers-fields
+	'( (:human-date     .  22)
+	   (:flags          .   6)
+	   (:mailing-list   .  10)
+	   (:from-or-to     .  22)
+	   (:thread-subject .  nil)))
+
+  ;; Message View settings
+  ;; show full addresses instead of just names
+  ;; toggle per name with M-RET
+  (setq mu4e-view-show-addresses 't)
+
+  ;; Enable org-mode capture for messages
+  (require 'org-mu4e))
+
 (use-package smtpmail
   :config
   (setq message-send-mail-function 'smtpmail-send-it
@@ -128,7 +217,11 @@
 	smtpmail-smtp-server "smtp.gmail.com"
 	smtpmail-smtp-service 587)
   ;; don't keep message buffers around
-  (setq message-kill-buffer-on-exit t))
+  (setq message-kill-buffer-on-exit t)
+  ;; Configure queue mode for offline mail queueing
+  (setq smtpmail-queue-mail nil  ;; start in queuing mode
+	smtpmail-queue-dir   "~/Mail/queue/cur")
+  )
 
 (load-theme 'misterioso)
 
